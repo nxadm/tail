5b4ba4f1bdde1b0f8cba0a3f1dc4e2bc78933f9f
Claudio Ramirez <pub.claudio@gmail.com>
2020-12-24 12:02:31 +0100

    add examples

---
+++ examples/tailAndPrint.go
@@ -0,0 +1,22 @@
+// Tail a file and print its contents.
+// In this example you can add lines to the syslog log by using the logger
+// command. Exit with Ctrl+C.
+package main
+
+import (
+	"fmt"
+	"github.com/nxadm/tail"
+)
+
+var logFile = "/var/log/syslog"
+
+func main() {
+	t, err := tail.TailFile(logFile, tail.Config{Follow: true})
+	if err != nil {
+		panic(err)
+	}
+
+	for line := range t.Lines {
+		fmt.Println(line.Text)
+	}
+}
--- go.mod
+++ go.mod
@@ -4,6 +4,6 @@ go 1.13
 
 require (
 	github.com/fsnotify/fsnotify v1.4.7
-	golang.org/x/sys v0.0.0-20190904154756-749cb33beabd // indirect
+	golang.org/x/sys v0.0.0-20200929083018-4d22bbb62b3c // indirect
 	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7
 )
