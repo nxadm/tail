1300da154a396c3f0c96a7bcb806c8c71b84a9c3
Claudio Ramirez <pub.claudio@gmail.com>
2021-02-06 19:50:53 +0100

    fmt

--- cmd/gotail/gotail.go
+++ cmd/gotail/gotail.go
@@ -7,7 +7,7 @@ package main
 import (
 	"flag"
 	"fmt"
-    "io"
+	"io"
 	"os"
 
 	"github.com/nxadm/tail"
@@ -53,14 +53,17 @@ func main() {
 
 func tailFile(filename string, config tail.Config, done chan bool) {
 	defer func() { done <- true }()
+
 	t, err := tail.TailFile(filename, config)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+
 	for line := range t.Lines {
 		fmt.Println(line.Text)
 	}
+
 	err = t.Wait()
 	if err != nil {
 		fmt.Println(err)
