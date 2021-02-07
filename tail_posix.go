ceb0606bb25f6a94bbb33c73d035f257fe2a825a
Claudio Ramirez <pub.claudio@gmail.com>
2021-02-07 15:03:13 +0100

    cc info

--- tail_posix.go
+++ tail_posix.go
@@ -1,3 +1,4 @@
+// Copyright (c) 2019 FOSS contributors of https://github.com/nxadm/tail
 // +build !windows
 
 package tail
@@ -6,6 +7,11 @@ import (
 	"os"
 )
 
+// OpenFile proxies a file os.Open so the file can be correctly tailed
+// on POSIX and non-POSIX OSes like MS Windows.
+//
+// This function is only useful internally and as such it will be
+// deprecated from the API in a future major release.
 func OpenFile(name string) (file *os.File, err error) {
 	return os.Open(name)
 }
--- tail_windows.go
+++ tail_windows.go
@@ -1,12 +1,19 @@
+// Copyright (c) 2019 FOSS contributors of https://github.com/nxadm/tail
 // +build windows
 
 package tail
 
 import (
-	"github.com/nxadm/tail/winfile"
 	"os"
+
+	"github.com/nxadm/tail/winfile"
 )
 
+// OpenFile proxies a file os.Open so the file can be correctly tailed
+// on POSIX and non-POSIX OSes like MS Windows.
+//
+// This function is only useful internally and as such it will be
+// deprecated from the API in a future major release.
 func OpenFile(name string) (file *os.File, err error) {
 	return winfile.OpenFile(name, os.O_RDONLY, 0)
 }
